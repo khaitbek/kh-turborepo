598a63275df96e33d7a22e82bf3ef7b4a3793fcc
khaitbek <khaitbek2005@gmail.com>
2023-06-25 12:25:44 +0500

    Initial commit

---
+++ apps/muslim-app/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "ui";
+
+export default function Home() {
+  return (
+    <>
+      <Button className="">
+        Click me!
+      </Button>
+      <h1 className="text-6xl ">Hello, Turborepo!</h1>
+    </>
+  )
+}
