e9d99465763764b9e56b0e26eb25aad9b88d8d38
khaitbek <khaitbek2005@gmail.com>
2023-07-13 23:28:58 +0500

    Fix env variables error: use bracket notation

--- apps/portfolio/drizzle.config.ts
+++ apps/portfolio/drizzle.config.ts
@@ -6,7 +6,7 @@ export default {
   out: "./src/server/migrations",
   breakpoints: true,
   dbCredentials: {
-    connectionString: process.env["DATABASE_URL"],
+    connectionString: process.env["DATABASE_URL"] as string,
   },
   driver: "mysql2",
 } satisfies Config;
