6aadc4cc82ee457348fc2ecda21771e803e1d5c9
khaitbek <khaitbek2005@gmail.com>
2023-08-02 15:24:12 +0500

    Make admin routes private

--- apps/portfolio/src/middleware.ts
+++ apps/portfolio/src/middleware.ts
@@ -4,9 +4,9 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/blog", "/projects", "blog/", "blog/1"],
+  publicRoutes: ["/", "/blog", "/projects", "blog/", "blog/$id"],
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/admin", "/(api|trpc)(.*)"],
+  matcher: ["/admin", "/(api|trpc)(.*)"],
 };
