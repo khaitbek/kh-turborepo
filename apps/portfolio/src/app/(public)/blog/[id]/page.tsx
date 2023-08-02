de8494df4fa41d5a2de1865d293781ad3d43c889
khaitbek <khaitbek2005@gmail.com>
2023-08-02 14:35:00 +0500

    Create a single blog page

--- apps/portfolio/src/app/(public)/blog/[id]/page.tsx
+++ apps/portfolio/src/app/(public)/blog/[id]/page.tsx
@@ -3,9 +3,8 @@ import { Section } from "@/components/ui/Section"
 import { db } from "@/server"
 import { Article, articles } from "@/server/schema/article"
 import { sql } from "drizzle-orm"
+import "github-markdown-css/github-markdown.css"
 import { ReactMarkdown } from "react-markdown/lib/react-markdown"
-import rehypeRaw from "rehype-raw"
-import remarkGfm from "remark-gfm"
 
 interface Props {
   params: {
@@ -15,15 +14,20 @@ interface Props {
 
 const BlogPostPage = async ({ params: { id } }: Props) => {
   const blogPost = (await db.execute(sql`select * from ${articles} where id=${id}`)).rows[0] as Article;
-
+  const markdown = "# Heading 1. # Hello world";
   return (
     <Section>
       <GradientHeading className="mb-6" as="h2">
         {blogPost.name}
       </GradientHeading>
-      <ReactMarkdown rehypePlugins={[rehypeRaw]} remarkPlugins={[remarkGfm]}>
-        {blogPost.body as string}
-      </ReactMarkdown>
+      <div className="markdown-body font-inter py-4 px-6 rounded-md">
+        <ReactMarkdown components={{
+          h1: "div",
+          strong: ({ node, ...props }) => <strong style={{ fontWeight: "800" }} {...props} />
+        }}>
+          {blogPost.body as string}
+        </ReactMarkdown>
+      </div>
     </Section>
   )
 }
